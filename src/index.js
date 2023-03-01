dbd9cc22746aaacd30fafc0dd8a440d752087466
ISIAKA ABDULAHI <56041462+isiakaabd@users.noreply.github.com>
2023-03-01 19:41:53 +0100

    added PWA

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
 import CssBaseline from "@mui/material/CssBaseline";
-
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
@@ -33,6 +33,7 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
